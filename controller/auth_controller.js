b82234ac02844602b57227d45f650f3fa4b48b1d
Okoro James <okorojameschizaram@gmail.com>
2023-06-27 12:08:33 +0100

    completed working on authentication

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -17,14 +17,12 @@ const registerController = async (req, res) => {
       });
 
       const adminEmail = AdminSchema.findOne({ email });
-      if (adminEmail) {
+      if (adminEmail)
         return res.status(400).json({ msg: "Email already exist" });
-      } else {
-        const salt = await bcrypt.genSalt(10);
-        adminInfo.password = await bcrypt.hash(password, salt);
-        await adminInfo.save();
-        return res.status(201).json(adminInfo);
-      }
+      const salt = await bcrypt.genSalt(10);
+      adminInfo.password = await bcrypt.hash(password, salt);
+      await adminInfo.save();
+      return res.status(201).json(adminInfo);
     }
   } catch (err) {
     console.log(err);
